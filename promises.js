271628e7b6f81428251dde3463f1db947dbb30a0
Pasan Sandaruwan <pasanudarasandaruwan11@gmail.com>
2026-02-16 13:33:25 +0530

    Add promises.js feature demonstrating basic Promise usage

---
+++ promises.js
@@ -0,0 +1,15 @@
+const condition = true; // Change to false to test rejection
+
+const myPromise = new Promise((resolve, reject) => {
+    if (condition) {
+        resolve('Success!');
+    } else {
+        reject('Failure!');
+    }
+});
+
+myPromise.then((result) => {
+    console.log(result);
+}).catch((error) => {
+    console.log(error);
+});
